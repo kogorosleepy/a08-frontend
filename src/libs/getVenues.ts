fa63f2775dad2c7246b4dfc8d8bc005db4461e78
kogorosleepy <bennyaphaa4869@gmail.com>
2026-03-18 21:52:59 +0700

    fixed try-catch

--- src/libs/getVenues.ts
+++ src/libs/getVenues.ts
@@ -1,14 +1,13 @@
 export default async function getVenues() {
-  const response = await fetch(
-    "https://a08-venue-explorer-backend.vercel.app/api/v1/venues",
-    { cache: "no-store" }
-  );
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch venues");
-  }
-
   try{
+    const response = await fetch(
+        "https://a08-venue-explorer-backend.vercel.app/api/v1/venues",
+        { cache: "no-store" }
+    );
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch venues");
+    }
     return await response.json();
   }catch(error){
     throw new Error("Invalid JSON response");
