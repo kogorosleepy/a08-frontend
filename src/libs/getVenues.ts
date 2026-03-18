0903d20cf7a608ef0b26b020a58eba3fd12234b8
kogorosleepy <bennyaphaa4869@gmail.com>
2026-03-18 22:27:34 +0700

    delete return

--- src/libs/getVenues.ts
+++ src/libs/getVenues.ts
@@ -12,59 +12,12 @@ const getVenues = async (): Promise<VenueJson> => {
     try {
       return JSON.parse(text);
     } catch {
-    //   throw new Error("Invalid JSON");
+      throw new Error("Invalid JSON");
     }
 
   } catch (error) {
-    // throw new Error("Invalid JSON response");
+    throw new Error("Invalid JSON response");
   }
-    return {
-    success: true,
-    count: 3,
-    pagination: {},
-    data: [
-      {
-        _id: "1",
-        name: "Mock Venue 1",
-        address: "Mock",
-        district: "Mock",
-        province: "Mock",
-        postalcode: "00000",
-        tel: "0000000000",
-        picture: "/img1.jpg",
-        dailyrate: 1000,
-        __v: 0,
-        id: "1",
-      },
-      {
-        _id: "2",
-        name: "Mock Venue 2",
-        address: "Mock",
-        district: "Mock",
-        province: "Mock",
-        postalcode: "00000",
-        tel: "0000000000",
-        picture: "/img2.jpg",
-        dailyrate: 2000,
-        __v: 0,
-        id: "2",
-      },
-      {
-        _id: "3",
-        name: "Mock Venue 3",
-        address: "Mock",
-        district: "Mock",
-        province: "Mock",
-        postalcode: "00000",
-        tel: "0000000000",
-        picture: "/img3.jpg",
-        dailyrate: 3000,
-        __v: 0,
-        id: "3",
-      },
-    ],
-  };
 };
-
 
 export default getVenues;
